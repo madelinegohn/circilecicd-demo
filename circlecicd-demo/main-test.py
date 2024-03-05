a8f175392883e408a46862741cb365fdfc6b52cf
Madeline <mg1406@messiah.edu>
2024-03-05 08:22:50 -0500

    modified the results

--- circlecicd-demo/main-test.py
+++ circlecicd-demo/main-test.py
@@ -1,8 +1,8 @@
 from main import Add
 
 def TestAdd():
-        assert Add(2,3) == 5
-        assert Add(5,5) == 10
+        assert Add(2,3) == 6
+        assert Add(5,5) == 25
         print("Add Function works correctly")
 
 if __name__ == '__main__':
